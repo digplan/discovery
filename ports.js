d6bc3387cd2385c24c581ab115dc2aa8d8aab301
digital planets <success@dpsw.info>
2015-01-17 13:06:55 -0600

    Create ports.js

---
+++ ports.js
@@ -0,0 +1,22 @@
+module.exports = ports = function(ip, begin, end, cb){
+  ip = ip || '127.0.0.1';
+  begin = begin-1;
+
+  var ret = {node: ip, openports: []};
+  
+  while(begin++ < end)
+  	tryport(ip, begin, ret);
+
+  setTimeout(function(){
+    cb(ret);
+  }, 4000);
+}
+
+function tryport(ip, port, ret){
+  var c = require('net').connect(port, ip);
+  var ip = ip, r = ret;
+  c.on('error', function(){});
+  c.on('connect', function(){ r.openports.push(port); c.end() });
+}
+
+ports('google.com', 80, 84, console.log.bind(console));
